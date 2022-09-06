cc7e06596e24041a2bb6020e415f2de802b875e9
mpdarshan1405 <mpdarshan1405@outlook.com>
2022-09-06 15:15:41 +0530

    i have created Hotel class

---
+++ Hotel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travelbooking
+{
+
+    public class Hotel
+    {
+        public string Continent { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string HotelName { get; set; }
+        public string Price { get; set; }
+
+        public Hotel(string continent, string country, string city, string hotelName, string price);
+        
+        {
+
+        this.Continent = continent;
+        this.Country = country;
+        this.City = city;
+        this.HotelName = hotelName;
+        this.Price = Convert.ToDouble(price);
+
+        }
+
+
+}
+}
+}
--- Program.cs
+++ Program.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Data;
 using System.Threading.Tasks;
 
-namespace travelbook
+namespace Travelbooking
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            string[] Filelines = File.ReadAllLines(@"C:\\Travel Agency Offers.tsv\");
+
+            List<Hotel> Hotels = new List<Hotel>();
+
+            foreach (var line in Filelines)
+            {
+                var recordArray = line.Split('\t');
+
+                Hotel hotel = new Hotel(recordArray[0], recordArray[1], recordArray[2], recordArray[3], recordArray[7]);
+
+                Hotels.Add(hotel);
+            }
+            Console.WriteLine("Continent=");
+
+            string? Continent = Console.ReadLine();
+
+            Console.Write("thanks");
         }
     }
 }
