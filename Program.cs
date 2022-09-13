771302cc17edac3b436f39bd57697ada86acb3fb
mpdarshan1405 <mpdarshan1405@outlook.com>
2022-09-13 10:38:48 +0530

    getting user input and splitting it using '='

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Data;
 using System.Threading.Tasks;
 using travelbook;
+using System.Globalization;
 
 namespace Travelbooking
 {
@@ -13,67 +14,74 @@ namespace Travelbooking
     {
         static void Main(string[] args)
         {
-            string[] Filelines = File.ReadAllLines(@"C:\Travel Agency Offers.tsv");
+            string[] Filelines = File.ReadAllLines(@"C:\Travel Agency Offers1.tsv");
 
-            List<Hotel>Hotels = new List<Hotel>();
+            List<Hotel>TripList = new List<Hotel>();
 
-            List<Hotel>Countries = new List<Hotel>(); 
 
             foreach (var line in Filelines ) 
             {
                 var recordArray = line.Split('\t');
-                //Console.WriteLine(recordArray[0] );
-                //Console.WriteLine(recordArray[1]);
-                //Console.WriteLine(recordArray[2]);
-                //Console.WriteLine(recordArray[3]);
-                //Console.WriteLine(recordArray[7]);
                 Hotel hotel = new Hotel(recordArray[0],recordArray[1],recordArray[2],recordArray[3],recordArray[7]);
-                Hotels.Add(hotel);
-                Hotel country = new Hotel(recordArray[0], recordArray[1], recordArray[2], recordArray[3], recordArray[7]);
-                Countries.Add(country);
+                TripList.Add(hotel);
+                
             }
 
 
-            //var asianHotels = Hotels.Where(x => x.Continent.Equals("Asia"));
+            Console.WriteLine("Welcome to TravelBooking");
 
-            //var europeanHotels = Hotels.Where(x => x.Continent.Equals("Europe"));
+            Console.WriteLine("Enter Your Desired Travel Continent/Country:");
 
-            Console.WriteLine("Enter Continent:");
 
-            string Continent = Console.ReadLine();
-
-            var asianCountries =  Countries.Where(x => x.Continent.Equals("Asia"));
-
-            var europeanCountries = Countries.Where(x => x.Continent.Equals("Europe"));
-
-            Console.WriteLine("List Of Countries:");
+            string userinput = Console.ReadLine();
+            var splitString = userinput.Split('=');
+            string userinputvalidate = splitString[0];
+            string userinputdata = splitString[1];
 
             
-            foreach (var country in asianCountries)
+
+            //Console.WriteLine(userinputvalidate);
+            //Console.WriteLine(userinputdata);
+            if(userinputvalidate == "Continent")
             {
-               
-                Console.WriteLine(country.Country);
+                var continentSpecificHotels = TripList.Where(r => r.Continent == userinputdata);
+                if (continentSpecificHotels.Count() != 0)
+                {
+                   // Console.WriteLine("data exists");
+                    int continentcount = continentSpecificHotels.Count();
+                    Console.WriteLine("Result For Continent= "+userinputdata+" ("+continentcount+")");
+                    int displaycount = 0;
+                    foreach (var Data in continentSpecificHotels)
+                    {
+                        Console.WriteLine("Continent: "+Data.Continent);
+                        Console.WriteLine("Country: " + Data.Country);
+                        Console.WriteLine("City: " + Data.City);
+                        Console.WriteLine("Hotel Name: " + Data.HotelName);
+                        Console.WriteLine("Price: " + Data.Price);
+                        displaycount++;
+                        
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No records found");
+                }
+            }
+            else if(userinputvalidate == "Country")
+            {
                 
-               
+                }
+                else
+                {
+                    Console.WriteLine("No records found");
+                }
             }
-
-            foreach (var country in europeanCountries)
-            {
-                Console.WriteLine(country.Country);
-            }
+           
 
 
 
-
-            //foreach (var hotel in asianHotels)
-            //{
-            //    Console.WriteLine(hotel.HotelName);
-            //}
-
-            //foreach (var hotel in europeanHotels)
-            //{
-            //    Console.WriteLine(hotel.HotelName);
-            //}
+            
+          
         }
     }
-}
